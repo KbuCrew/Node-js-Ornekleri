df721f687f59a23d9f4614052796cca1dad19eaf
mbilalbark <muh.bark@gmail.com>
2018-11-11 01:43:05 +0300

    Fix: Sytax Fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // foreach exapmle
 
-var tatlilar = ['kek','baklava','aaaa'];
+var tatlilar = ['kek','baklava','tiramisu','trilice','kabak','köstebek','sütlaç','kazandibi','malaga'];
 
 tatlilar.forEach(element => {
     console.log(element)
